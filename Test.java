f1cfe13edb16e2bca84264c95795acbe87056a26
Caesar Vior <viorcaesar@gmail.com>
2025-10-02 15:44:59 +0700

    menambahkan role baru

--- Test.java
+++ Test.java
@@ -2,12 +2,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama yang dimodifikasi User 2");
         System.out.println("Caesar_Vior_Absen06 menambahkan baris baru");
-<<<<<<< HEAD
         System.out.println("Hello's there, Kiyah's here");
         System.out.println("Now its raffi's turn");
-=======
         System.out.println("Caesar adalah seorang programmer");
->>>>>>> c07278a (Menambahkan role caesar)
     }
 
 }
