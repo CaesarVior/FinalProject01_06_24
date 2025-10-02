53617fbbd78d81c7bb563b73771dd80ed4d8eb53
Caesar Vior <viorcaesar@gmail.com>
2025-10-02 15:03:26 +0700

    Menambahkan baris caesar baru

--- Test.java
+++ Test.java
@@ -1,8 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama yang dimodifikasi User 2");
-        System.out.println("Caesar_Vior_06 menambahkan baris baru");
-
+        System.out.println("Caesar_Vior_Absen06 menambahkan baris baru");
     }
 
 }
