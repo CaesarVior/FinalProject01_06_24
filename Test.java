7cc1454451c24bbd61a65e21ea82bcdb543cf268
borzooraa <nurfakiyahr24@gmail.com>
2025-10-02 15:54:29 +0700

    Kiyah nyoba conflict

--- Test.java
+++ Test.java
@@ -3,8 +3,7 @@ public class Test {
         System.out.println("Hi, ini baris kode pertama yang dimodifikasi User 2");
         System.out.println("Caesar_Vior_Absen06 menambahkan baris baru");
         System.out.println("Hello's there, Kiyah's here");
-        System.out.println("Now its raffi's turn");
-        System.out.println("Caesar adalah seorang programmer");
+        System.out.println("Kiyah's love RMA");
     }
 
 }
